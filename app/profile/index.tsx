67f5ce8cabd55690408bf6e8f437876c7756920d
Kirill Tchentsov <kirill.develops@gmail.com>
2026-04-20 12:33:36 -0400

    updated Profile page to show welcome message

--- app/profile/index.tsx
+++ app/profile/index.tsx
@@ -1,5 +1,15 @@
-import { View } from "react-native";
+import { Screen, Subtitle, Title } from "@/components/UI";
+import { useAuth } from "@/context/AuthContext";
 
-export default function index() {
-   return <View></View>;
+export default function Index() {
+   const { session } = useAuth();
+
+   return (
+      <Screen>
+         <Title>Welcome back</Title>
+         <Subtitle>
+            {!!session && `Welcome back, ${session.user.email}!`}
+         </Subtitle>
+      </Screen>
+   );
 }
