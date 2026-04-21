d735244da1b1b0734ecd2b9d18dc3f012d98a2c0
Kirill Tchentsov <kirill.develops@gmail.com>
2026-04-21 19:16:33 -0400

    refactored to identify password field and password visibility state

--- components/InputGroup.tsx
+++ components/InputGroup.tsx
@@ -1,16 +1,86 @@
-import { Input, Label } from "@/components/UI";
+import { EyeClosed, EyeOpen, Input, Label } from "@/components/UI";
 import theme from "@/theme";
-import { TextInputProps, View } from "react-native";
+import { ReactNode, useState } from "react";
+import { Pressable, StyleSheet, TextInputProps, View } from "react-native";
 
 type InputGroupProps = TextInputProps & {
    label: string;
+   rightActionLabel?: ReactNode;
+   onRightActionPress?: () => void;
 };
 
-export default function InputGroup({ label, ...inputProps }: InputGroupProps) {
+export default function InputGroup({
+   label,
+   rightActionLabel,
+   style,
+   textContentType,
+   ...inputProps
+}: InputGroupProps) {
+   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+   const isPassword = textContentType === "newPassword";
+
+   const togglePasswordVisibility = () => {
+      setIsPasswordVisible((visible) => !visible);
+   };
+
    return (
-      <View style={{ marginBottom: theme.spacing.marginBottomInputGroup }}>
+      <View style={styles.InputGroup}>
          <Label>{label}</Label>
-         <Input {...inputProps} />
+         <View style={styles.inputWrapper}>
+            <Input
+               secureTextEntry={!!isPassword && !isPasswordVisible}
+               textContentType={textContentType}
+               {...inputProps}
+               style={[!!isPassword && styles.passwordInput, style]}
+            />
+            {isPassword && (
+               <PasswordVisabilityToggle
+                  isVisable={isPasswordVisible}
+                  onPress={togglePasswordVisibility}
+               >
+                  {rightActionLabel}
+               </PasswordVisabilityToggle>
+            )}
+         </View>
       </View>
    );
 }
+
+type PasswordVisabilityToggleProps = {
+   onPress: () => void;
+   children: ReactNode;
+   isVisable: boolean;
+};
+
+function PasswordVisabilityToggle({
+   onPress,
+   isVisable,
+}: PasswordVisabilityToggleProps) {
+   return (
+      <Pressable
+         onPress={onPress}
+         style={styles.eyeIcon}
+      >
+         {isVisable ? <EyeClosed /> : <EyeOpen />}
+      </Pressable>
+   );
+}
+
+const styles = StyleSheet.create({
+   InputGroup: {
+      marginBottom: theme.spacing.marginBottomInputGroup,
+   },
+   inputWrapper: {
+      position: "relative",
+   },
+   passwordInput: {
+      paddingRight: 52,
+   },
+   eyeIcon: {
+      position: "absolute",
+      right: theme.spacing.inputPaddingHorizontal,
+      top: 0,
+      bottom: 0,
+      justifyContent: "center",
+   },
+});
